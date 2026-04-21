3b438ad74e1bce87d2ed91a44259cec792e1cd6a
Varvara Vekiou <165425751+Veravekiou@users.noreply.github.com>
2026-04-21 23:13:19 +0300

    Prepare project for final submission

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -41,7 +41,6 @@ export default function RootLayout() {
             }
           }}
         />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
       </Stack>
       <StatusBar style="auto" />
     </ThemeProvider>
